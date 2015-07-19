53d4ade80ce0d176f1e9f9d52391d559f3f3250c
Sheng.Li <eric@lishengcn.cn>
2015-07-19 16:43:47 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,28 @@
 /**
  * Created by lisheng on 15/7/19.
  */
+
+var gulp = require('gulp'),
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  minifycss = require('gulp-minify-css'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  clean = require('gulp-clean'),
+  concat = require('gulp-concat'),
+  notify = require('gulp-notify'),
+  cache = require('gulp-cache'),
+  livereload = require('gulp-livereload');
+
+
+gulp.task('styles', function() {
+  return gulp.src('stylesheets/screen.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('stylesheets'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('stylesheets'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
